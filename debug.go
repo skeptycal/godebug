9a32b3d0ebbc638bd30c9723315205eec3bf20f1
skeptycal <skeptycal@gmail.com>
2020-12-04 00:36:23 -0600

    refactor

--- debug.go
+++ debug.go
@@ -1,4 +1,4 @@
-package debug
+package godebug
 
 import (
 	"log"
@@ -10,23 +10,64 @@ func init() {
 	log.Println("init in debug.go")
 }
 
+// VerboseLevel constants describe the level of output and logging.
+// Output will be every category where ("verbose setting variable" >= VerboseLevel)
+//
+//      TRACE VerboseLevel = 5
+//      DEBUG VerboseLevel = 10
+//      INFO VerboseLevel = 20
+//      SUCCESS VerboseLevel = 25
+//      WARNING VerboseLevel = 30
+//      ERROR VerboseLevel = 40
+//      CRITICAL VerboseLevel = 50
+//
+// e.g.
+//      DEBUG will output all debug info, successes, warnings, errors
+//      ERROR will output only error info
+//      SUCCESS will output only success info and above (warnings,etc.)
+type VerboseLevel int8
+
+const (
+	// TRACE - Output every dam thing
+	TRACE VerboseLevel = 5
+	// DEBUG - Output all including debug info
+	DEBUG VerboseLevel = 10
+	// INFO - Output standard information
+	INFO VerboseLevel = 20
+	// SUCCESS - Output successful task and errors
+	SUCCESS VerboseLevel = 25
+	// WARNING - Output all nonfatal warnings and errors
+	WARNING VerboseLevel = 30
+	// ERROR - Output only Fatal errors
+	ERROR VerboseLevel = 40
+	// CRITICAL - Output only Panic errors
+	CRITICAL VerboseLevel = 50
+)
+
+type devMode bool
+
+// devMode returns true if the build is in dev mode
+const (
+	production  devMode = false
+	development devMode = true
+)
+
 // Session defines session information for DEV or PRODUCTION modes
 type Session struct {
 	Name         string
-	IsDevMode    bool
-	UseLogger    bool
+	IsDevMode    devMode // DEV or PRODUCTION
+	UseLogger    bool    // use Log?
 	Verbose      VerboseLevel
 	sessionStart time.Time
 	sessionEnd   time.Time
 	userID       int
-	defaults     defaultValues
 }
 
-func (s *Session) init(name string, mode bool, useLogger bool, useVerbose VerboseLevel) {
+func (s *Session) init(name string, mode devMode, useLogger bool, verbose VerboseLevel) {
 	s.Name = name
 	s.IsDevMode = mode
 	s.UseLogger = useLogger
-	s.Verbose = useVerbose
+	s.Verbose = verbose
 	s.sessionStart = time.Now()
 	s.userID = os.Getuid()
 }
@@ -40,6 +81,7 @@ func logPrint(v ...interface{}) {
 }
 
 var defaultConfig Session
+var defaultValues Session
 
 func init() {
 	defaultConfig.init("anansi", true, false, verboseAll)
